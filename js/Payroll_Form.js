ea08406e49e0eedfa7e97fef15f62566d61ff648
mihirpatil0 <mihirrpatil@gmail.com>
2021-08-10 22:33:37 +0530

    [mihir] Added: event listener on salary field.

--- js/Payroll_Form.js
+++ js/Payroll_Form.js
@@ -1,3 +1,7 @@
-/*
-This is employee payroll form .js file
-*/
+const salary = document.querySelector('#salary')
+const output = document.querySelector('.salary-output')
+output.textContent = salary.value;
+salary.addEventListener('input', function ()
+{
+       output.textContent = salary.value
+});
