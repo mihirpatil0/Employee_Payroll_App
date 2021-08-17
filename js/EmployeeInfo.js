9bf4ed075110a84090370257b61edb899789b0af
mihirpatil0 <mihirrpatil@gmail.com>
2021-08-18 00:46:24 +0530

    [mihir] Refactored: changed id getter and setter.

--- js/EmployeeInfo.js
+++ js/EmployeeInfo.js
@@ -1,14 +1,7 @@
 class EmployeePayrollData
 {
-    get id()
-    {
-        return this._id;
-    }
-    set id(id)
-    {
-        this._id = id;
-    }
-     
+    id;
+        
     get name()
     {
         return this._name;
